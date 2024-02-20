2a76d3bcee4a0e14569f08d08b723ee7d383d5ad
Heyitstyler <149159183+Heyitstyler@users.noreply.github.com>
2024-02-20 07:46:39 -0700

    Add files via upload

--- install.bat
+++ install.bat
@@ -3,4 +3,17 @@ cd Scripts
 call activate.bat
 cd ..
 pip install -r requirements.txt
-pause
+
+@echo off
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "Run.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%cd%\init.bat" >> %SCRIPT%
+echo oLink.WindowStyle = 7 >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
